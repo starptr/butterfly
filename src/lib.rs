f7040363c02b556fe4a5c4436998112e58f27904
Yuto Nishida <yuto@berkeley.edu>
2023-03-29 16:50:52 -0700

    Working get endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -48,15 +48,15 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
     // functionality and a `RouteContext` which you can use to  and get route parameters and
     // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
-    let router = attach_get_common_permanent_redirect!(router, "/tiktok", "https://tiktok.com/@really_yuto");
-    let router = attach_get_common_permanent_redirect!(router, "/twitter", "https://twitter.com/really_yuto");
-    let router = attach_get_common_permanent_redirect!(router, "/instagram", "https://instagram.com/really_yuto/");
-    let router = attach_get_common_permanent_redirect!(router, "/ig", "https://instagram.com/really_yuto/");
-    let router = attach_get_common_permanent_redirect!(router, "/linkedin", "https://linkedin.com/in/yuton/");
-    let router = attach_get_common_permanent_redirect!(router, "/facebook", "https://facebook.com/yuto314/");
-    let router = attach_get_common_permanent_redirect!(router, "/fb", "https://facebook.com/yuto314/");
+    //let router = attach_get_common_permanent_redirect!(router, "/tiktok", "https://tiktok.com/@really_yuto");
+    //let router = attach_get_common_permanent_redirect!(router, "/twitter", "https://twitter.com/really_yuto");
+    //let router = attach_get_common_permanent_redirect!(router, "/instagram", "https://instagram.com/really_yuto/");
+    //let router = attach_get_common_permanent_redirect!(router, "/ig", "https://instagram.com/really_yuto/");
+    //let router = attach_get_common_permanent_redirect!(router, "/linkedin", "https://linkedin.com/in/yuton/");
+    //let router = attach_get_common_permanent_redirect!(router, "/facebook", "https://facebook.com/yuto314/");
+    //let router = attach_get_common_permanent_redirect!(router, "/fb", "https://facebook.com/yuto314/");
     router
-        .get("/", |_, _| Response::ok("Hello from Workers!"))
+        //.get("/", |_, _| Response::ok("Hello from Workers!"))
         .post_async("/form/:field", |mut req, ctx| async move {
             if let Some(name) = ctx.param("field") {
                 let form = req.form_data().await?;
@@ -113,10 +113,41 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
                 Err(e) => Response::error(format!("Json parsing failed: {}", e), 400),
             }
         })
-        .get("/worker-version", |_, ctx| {
-            let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
-            Response::ok(version)
+        .get_async("/:slug", |mut req, ctx| async move {
+            if let Some(slug) = ctx.param("slug") {
+                let kv = ctx.kv("KV_FROM_RUST");
+                if kv.is_err() {
+                    return Response::error("failed to retrieve KV store", 500);
+                }
+                let kv = kv.unwrap();
+                let get_builder = kv.get(slug);
+                let url = get_builder.text().await;
+                if url.is_err() {
+                    return Response::error(format!("failed to retrieve KV store: {}", url.unwrap_err()), 500);
+                }
+                let url: Option<String> = url.unwrap();
+                if url.is_none() {
+                    return Response::error(format!("no value for {}", slug), 500);
+                }
+                let url: String = url.unwrap();
+                let mut parsed_url = Url::parse(&url);
+                if parsed_url.is_err() {
+                    let with_protocol = "https://".to_owned() + &url;
+                    let parsed_url_with_protocol = Url::parse(&with_protocol);
+                    if parsed_url_with_protocol.is_err() {
+                        return Response::error(format!("failed to parse url: {}", with_protocol), 500);
+                    }
+                    parsed_url = parsed_url_with_protocol;
+                }
+                let url = parsed_url.unwrap();
+                return Response::redirect_with_status(url, 301);
+            }
+            Response::error("Bad Request", 400)
         })
+        //.get("/worker-version", |_, ctx| {
+        //    let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
+        //    Response::ok(version)
+        //})
         .run(req, env)
         .await
 }
